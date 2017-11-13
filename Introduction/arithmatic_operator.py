3165823d6da60c167da288b62dbe82b26b1c4528
akik22 <akik22@gmail.com>
2017-11-13 22:41:53 +0600

    Arithmatic Operator completed

---
+++ Introduction/arithmatic_operator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+first_number = int(input())
+second_number = int(input())
+
+print(first_number+second_number)
+print(first_number-second_number)
+print(first_number*second_number)
