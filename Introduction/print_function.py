574f30aeb26e7edd8e211fb044ebf120ac7d69d7
akik22 <akik22@gmail.com>
2017-11-13 23:04:40 +0600

    Introduction Completed

---
+++ Introduction/print_function.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+given_number = int(input())
+
+print(*(range(1,given_number+1)),sep='')
---
+++ Introduction/write_a_function.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+import calendar
+
+def is_leap(year):
+    return calendar.isleap(year)
